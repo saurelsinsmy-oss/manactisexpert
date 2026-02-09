c315933b7bca32a2cbb5491a7395ca1431cbea86
Sinsmy Saurel <saurelsinsmy@gmail.com>
2026-02-08 19:28:39 -0500

    Add form submission alert message

---
+++ script.js
@@ -0,0 +1,5 @@
+// Interaction simple pour le formulaire
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Merci pour votre message ! Nous vous répondrons rapidement.");
+});
